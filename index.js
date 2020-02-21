85847b234a8677668b2ac7c5f47a256cf03cddd3
Rafael Dantas <rafael.dantas@omniti.com.br>
2020-02-20 22:12:43 -0300

    feat: add input robot and state robot

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-const readline = require('readline-sync')
 const robots = {
-    text: require('./robots/text')
+    input: require('./robots/input'),
+    text: require('./robots/text'),
+    state: require('./robots/state')
 }
 
 async function start() {
-    const content = {
-        maximumSentences: 7
-    }
+    robots.input()
+    await robots.text()
 
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
-
-    await robots.text(content)
-
-    function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipedia search term: ')
-    }
-
-    function askAndReturnPrefix() {
-        const prefixes = ['Who is', 'What is', 'The history of']
-        return prefixes[readline.keyInSelect(prefixes, 'Choose one option')]
-    }
-
-    console.log(content)    
+    const content = robots.state.load()
+    console.dir(content, { depth: null })
 }
 
 start()
